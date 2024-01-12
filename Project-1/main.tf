29fe6055025b26937f07b8e43c6f9b00ece983bb
Johnson <johnsonfavor2016@gmail.com>
2024-01-12 15:11:25 -0600

    Added tf files

---
+++ Project-1/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.grace.id
+  instance_type = "t2.micro"
+
+
+  tags = {
+    Name = "Favor"
+  }
+}
+
+data "aws_ami" "grace" {
+  most_recent = true
+}
+
+
+resource "aws_iam_user" "access" {
+  name = "devops"
+}
